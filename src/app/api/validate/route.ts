36a557f678b282ea2cae6120d72df354d4a7bd80
Mateus.nogueira-extern <mateus.nogueira-extern@renault.com>
2024-12-10 10:20:56 -0300

    fix: change get response

--- src/app/api/validate/route.ts
+++ src/app/api/validate/route.ts
@@ -1,25 +1,25 @@
-import { consultaChavePix } from "@/lib/dictApi";
-import { NextRequest, NextResponse } from "next/server";
+import { consultaChavePix } from '@/lib/dictApi'
+import { NextRequest, NextResponse } from 'next/server'
 
 export async function GET(req: NextRequest) {
-  const { searchParams } = req.nextUrl;
-  const pixKey = searchParams.get("pixKey");
-  console.log("pixKey", pixKey);
+  const { searchParams } = req.nextUrl
+  const pixKey = searchParams.get('pixKey')
+  console.log('pixKey', pixKey)
 
   if (!pixKey) {
-    return {
-      status: 400,
-      json: { error: "pixKey query parameter is required" },
-    };
+    return NextResponse.json(
+      { error: 'Pix key query parameter is required' },
+      { status: 400 }
+    )
   }
 
   try {
-    const response = await consultaChavePix(pixKey);
-    return NextResponse.json(response, { status: 200 });
+    const response = await consultaChavePix(pixKey)
+    return NextResponse.json(response, { status: 200 })
   } catch (error) {
     return NextResponse.json(
-      { error: "An error occurred while fetching Pix key" },
+      { error: 'An error occurred while fetching Pix key' },
       { status: 500 }
-    );
+    )
   }
 }
