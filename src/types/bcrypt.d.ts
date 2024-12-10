074b20c88eccb45c6057477a25f20efe089a8e6b
Mateus.nogueira-extern <mateus.nogueira-extern@renault.com>
2024-12-10 10:35:59 -0300

    feat: add bcrypt type

---
+++ src/types/bcrypt.d.ts
@@ -0,0 +1,18 @@
+declare module 'bcrypt' {
+  export function hash(
+    data: string | Buffer,
+    saltOrRounds: string | number
+  ): Promise<string>
+  export function hashSync(
+    data: string | Buffer,
+    saltOrRounds: string | number
+  ): string
+  export function compare(
+    data: string | Buffer,
+    encrypted: string
+  ): Promise<boolean>
+  export function compareSync(
+    data: string | Buffer,
+    encrypted: string
+  ): boolean
+}
