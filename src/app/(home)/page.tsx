17ebd6495b2f24f31b71a101398045c993e38e0e
Mateus.nogueira-extern <mateus.nogueira-extern@renault.com>
2024-12-10 13:02:13 -0300

    fix: adjusts server components

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -3,15 +3,17 @@ import { getSession } from '@/lib/session'
 import { getLastTransactions } from '@/services/get-transactions.service'
 
 export default async function HomePage() {
+  
   const session = await getSession()
-
-  const lastTransactions = await getLastTransactions({
-    userId: session?.user.id as string
-  })
 
   if (!session) {
     return null
   }
+  
+  const lastTransactions = await getLastTransactions({
+    userId: session?.user.id as string
+  })
+
   return (
     <div className="flex h-screen flex-col-reverse justify-end gap-6 p-4 lg:flex-row">
       <LastTransactions
