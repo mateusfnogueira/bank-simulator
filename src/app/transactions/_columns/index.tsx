3b8067c2328170e008f82993316ac29838b68061
Mateus.nogueira-extern <mateus.nogueira-extern@renault.com>
2024-12-10 09:15:25 -0300

    feat: add new recipient column

--- src/app/transactions/_columns/index.tsx
+++ src/app/transactions/_columns/index.tsx
@@ -1,33 +1,51 @@
 'use client'
-import { ITransaction } from "@/interfaces/transactions.interface";
-import { formatCurrency } from "@/utils/currency.util";
-import { ColumnDef } from "@tanstack/react-table";
-
+import { ITransaction } from '@/interfaces/transactions.interface'
+import { formatCurrency } from '@/utils/currency.util'
+import { ColumnDef } from '@tanstack/react-table'
 
 export const transactionColumns: ColumnDef<ITransaction>[] = [
-
   {
     header: 'Título',
-    accessorKey: 'title',
+    accessorKey: 'title'
+  },
+  {
+    header: 'Destinatário',
+    accessorKey: 'recipient',
+    cell: ({ row: { original: trasaction } }) =>
+      setRecipient(trasaction)
   },
   {
     header: 'Tipo',
     accessorKey: 'type',
-    cell: ({row: {original: trasaction}}) => trasaction.type === 'INCOME' ? 'Entrada' : 'Saída', 
+    cell: ({ row: { original: trasaction } }) =>
+      trasaction.type === 'INCOME' ? 'Entrada' : 'Saída'
   },
   {
     header: 'Valor',
     accessorKey: 'amount',
-    cell: ({row: {original: trasaction}}) => formatCurrency(trasaction.amount), 
+    cell: ({ row: { original: trasaction } }) =>
+      formatCurrency(trasaction.amount)
   },
   {
     header: 'Data de criação',
     accessorKey: 'createdAt',
-        cell: ({ row: { original: transaction } }) =>
-      new Date(transaction.createdAt).toLocaleDateString('pt-BR', {
-        day: '2-digit',
-        month: 'long',
-        year: 'numeric'
-      })
-  },
+    cell: ({ row: { original: transaction } }) =>
+      new Date(transaction.createdAt || '').toLocaleDateString(
+        'pt-BR',
+        {
+          day: '2-digit',
+          month: 'long',
+          year: 'numeric'
+        }
+      )
+  }
 ]
+
+function setRecipient(transaction: ITransaction) {
+  if (transaction.category === 'PIX') {
+    return 'PIX para ' + transaction.recipient
+  } else if (transaction.category === 'TED') {
+    return 'TED para ' + transaction.recipient
+  }
+  return transaction.recipient || '-'
+}
