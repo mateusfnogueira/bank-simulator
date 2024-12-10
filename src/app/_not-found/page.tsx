838911da8a682f239ebadc6d562fe7ccfbccb975
Mateus.nogueira-extern <mateus.nogueira-extern@renault.com>
2024-12-10 10:46:04 -0300

    feat: create not found page

---
+++ src/app/_not-found/page.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <div>
+      <h1>404 - Página Não Encontrada</h1>
+      <p>A página que você está procurando não existe.</p>
+    </div>
+  )
+}
