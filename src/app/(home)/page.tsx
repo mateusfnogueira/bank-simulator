92144c877510d3b17c499df864bf32051995578e
Mateus.nogueira-extern <mateus.nogueira-extern@renault.com>
2024-12-10 19:57:21 -0300

    fix: remove try catch

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -3,24 +3,19 @@ import { getSession } from "@/lib/session";
 import { getLastTransactions } from "@/services/get-transactions.service";
 
 export default async function HomePage() {
-  try {
-    const session = await getSession();
-    if (!session) {
-      return null;
-    }
-
-    const lastTransactions = await getLastTransactions({
-      userId: session?.user.id as string,
-    });
-
-    return (
-      <div className="flex h-screen flex-col-reverse justify-end gap-6 p-4 lg:flex-row">
-        <LastTransactions lastTransactions={lastTransactions.transactions} />
-        <TotalBalanceCard session={session} />
-      </div>
-    );
-  } catch (e) {
-    console.log(e);
+  const session = await getSession();
+  if (!session) {
     return null;
   }
+
+  const lastTransactions = await getLastTransactions({
+    userId: session?.user.id as string,
+  });
+
+  return (
+    <div className="flex h-screen flex-col-reverse justify-end gap-6 p-4 lg:flex-row">
+      <LastTransactions lastTransactions={lastTransactions.transactions} />
+      <TotalBalanceCard session={session} />
+    </div>
+  );
 }
